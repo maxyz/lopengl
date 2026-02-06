de3f292a8a150a8e0fa130d23e01832696b5316e
Margarita Manterola <margamanterola@gmail.com>
2026-02-06 13:49:48 +0100

    add the rest of the code for hello window

--- marga/src/1-hellowindow/1-hellowindow.cpp
+++ marga/src/1-hellowindow/1-hellowindow.cpp
@@ -7,6 +7,12 @@ void framebuffer_size_callback(GLFWwindow* window, int width, int height)
     glViewport(0, 0, width, height);
 }
 
+void processInput(GLFWwindow *window)
+{
+    if(glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+        glfwSetWindowShouldClose(window, true);
+}
+
 int main()
 {
     glfwInit();
@@ -38,8 +44,15 @@ int main()
     // Render loop
     while(!glfwWindowShouldClose(window))
     {
+        // input
+        processInput(window);
+        // rendering commands here
+        glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        // check and call events and swap buffers
+        glfwPollEvents();         
         glfwSwapBuffers(window);
-        glfwPollEvents();    
     }
 
     // Clean up
