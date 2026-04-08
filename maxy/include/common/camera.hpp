aa8b7946766b40f830c4c2f37b747fb9a5f1dd9a
Maximiliano Curia <maxy@gnuservers.com.ar>
2026-04-08 18:24:57 +0200

    Camera class

---
+++ maxy/include/common/camera.hpp
@@ -0,0 +1,95 @@
+#pragma once
+#include <algorithm>
+
+#include <glad/gl.h>
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+
+enum CameraMovement {
+  FORWARD,
+  BACKWARD,
+  LEFT,
+  RIGHT,
+  UP,
+  DOWN,
+};
+
+const float SPEED = 2.5f;
+const float SENSITIVITY = 0.1f;
+const float FOV = 45.f;
+
+class Camera {
+public:
+  glm::vec3 position;
+  glm::vec3 front;
+  glm::vec3 up;
+  glm::vec3 right;
+  glm::vec3 world_up;
+  float yaw;
+  float pitch;
+  float fov = FOV;
+  float speed = SPEED;
+  float sensitivity = SENSITIVITY;
+
+  Camera(glm::vec3 position = glm::vec3(0.f, 0.f, 0.f), float yaw = -90.f,
+         float pitch = 0.f)
+      : position(position), yaw(yaw), pitch(pitch),
+        world_up(glm::vec3(0.f, 1.f, 0.f)) {
+    update_vectors();
+  }
+
+  glm::mat4 get_view_matrix() {
+    return glm::lookAt(position, position + front, up);
+  }
+
+  void process_movement(CameraMovement direction, float delta) {
+    float velocity = speed * delta;
+    switch (direction) {
+    case FORWARD:
+      position += front * velocity;
+      break;
+    case BACKWARD:
+      position -= front * velocity;
+      break;
+    case LEFT:
+      position -= right * velocity;
+      break;
+    case RIGHT:
+      position += right * velocity;
+      break;
+    case UP:
+      position += up * velocity;
+      break;
+    case DOWN:
+      position -= up * velocity;
+      break;
+    }
+  }
+
+  void process_rotation(float x_offset, float y_offset,
+                        bool constraint_pitch = true) {
+    x_offset *= sensitivity;
+    y_offset *= sensitivity;
+
+    yaw += x_offset;
+    pitch += y_offset;
+
+    if (constraint_pitch) {
+      pitch = std::clamp(pitch, -89.f, 89.f);
+    }
+    update_vectors();
+  }
+
+  void update_fov(float delta) { fov = std::clamp(fov + delta, 1.f, 90.f); }
+
+private:
+  void update_vectors() {
+    glm::vec3 f;
+    f.x = cos(glm::radians(yaw)) * cos(glm::radians(pitch));
+    f.y = sin(glm::radians(pitch));
+    f.z = sin(glm::radians(yaw)) * cos(glm::radians(pitch));
+    front = glm::normalize(f);
+    right = glm::normalize(glm::cross(front, world_up));
+    up = glm::normalize(glm::cross(right, front));
+  }
+};
