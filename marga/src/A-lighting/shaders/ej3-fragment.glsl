fe2b95016c6b0bee233037343ce4fd9a48e679d7
Margarita Manterola <margamanterola@gmail.com>
2026-04-17 12:07:48 +0200

    Ej3

---
+++ marga/src/A-lighting/shaders/ej3-fragment.glsl
@@ -0,0 +1,37 @@
+#version 330 core
+
+in vec3 FragPos;
+in vec3 Normal;
+in vec3 LightPos;
+
+out vec4 FragColor;
+  
+uniform vec3 objectColor;
+uniform vec3 lightColor;
+
+void main()
+{
+    // Ambient light
+    float ambientStrength = 0.1;
+    vec3 ambient = ambientStrength * lightColor;
+
+    // Calculate the angle of the light, for diffuse lighting
+    vec3 norm = normalize(Normal);
+    vec3 lightDir = normalize(LightPos - FragPos);
+    // Calculate the impact of the light
+    float diff = max(dot(norm, lightDir), 0.0);
+    vec3 diffuse = diff * lightColor;
+
+    // Specular light
+    float specularStrength = 0.5;
+    // Calculates angles of reflection and view
+    vec3 viewDir = normalize(-FragPos);
+    vec3 reflectDir = reflect(-lightDir, norm);
+    // Calculate the specular component
+    float shininess = 32.0;
+    float spec = pow(max(dot(viewDir, reflectDir), 0.0), shininess);
+    vec3 specular = specularStrength * spec * lightColor; 
+
+    vec3 result = (ambient + diffuse + specular) * objectColor;
+    FragColor = vec4(result, 1.0);
+}
