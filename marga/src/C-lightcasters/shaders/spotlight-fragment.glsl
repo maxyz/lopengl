678dcc57b6bd7b10e5469d536b01262ccedc351b
Margarita Manterola <margamanterola@gmail.com>
2026-04-25 15:35:03 +0200

    Spotlight

---
+++ marga/src/C-lightcasters/shaders/spotlight-fragment.glsl
@@ -0,0 +1,62 @@
+#version 330 core
+
+in vec2 TexCoords;
+
+struct Material {
+    sampler2D diffuse;
+    sampler2D specular;
+    float     shininess;
+};
+uniform Material material;
+
+struct Light {
+    vec3 position;
+    vec3 direction;
+    vec3 ambient;
+    vec3 diffuse;
+    vec3 specular;
+
+    float constant;
+    float linear;
+    float quadratic;
+    float cutOff;
+    float outerCutOff;
+};
+uniform Light light; 
+
+uniform vec3 viewPos;
+
+in vec3 FragPos;
+in vec3 Normal;
+
+out vec4 FragColor;
+  
+void main()
+{
+    // Calculate the angle of the light, for diffuse lighting
+    vec3 norm = normalize(Normal);
+    vec3 lightDir = normalize(light.position - FragPos);
+    // Calculate the impact of the light
+    float diff = max(dot(norm, lightDir), 0.0);
+    // Calculates angles of reflection and view for specular light
+    vec3 viewDir = normalize(viewPos - FragPos);
+    vec3 reflectDir = reflect(-lightDir, norm);
+    // Calculate the specular component
+    float spec = pow(max(dot(viewDir, reflectDir), 0.0), material.shininess);
+    
+    vec3 diffuse = light.diffuse * diff * vec3(texture(material.diffuse, TexCoords));
+    vec3 ambient = light.ambient * vec3(texture(material.diffuse, TexCoords));
+    vec3 specular = light.specular * spec * vec3(texture(material.specular, TexCoords));
+
+    // Attenuation
+    float distance = length(light.position - FragPos);
+    float attenuation = 1.0 / (light.constant + light.linear * distance + 
+    		    light.quadratic * (distance * distance));
+
+    // Cut off
+    float theta     = dot(lightDir, normalize(-light.direction));
+    float epsilon   = light.cutOff - light.outerCutOff;
+    float intensity = clamp((theta - light.outerCutOff) / epsilon, 0.0, 1.0);
+    vec3 result = (ambient + (diffuse + specular)*intensity) * attenuation;
+    FragColor = vec4(result, 1.0);
+}
