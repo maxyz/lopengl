aac2a0ea05808ef7d91bdd113b8640a1d885a485
Margarita Manterola <margamanterola@gmail.com>
2026-05-15 10:43:32 +0200

    Depth testing

--- marga/src/lib/texture.cpp
+++ marga/src/lib/texture.cpp
@@ -11,11 +11,18 @@ Texture::Texture(const char* filename, GLenum format)
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);    
     // Load the image to use as texture
-    int width, height, nrChannels;
-    unsigned char *data = stbi_load(filename, &width, &height, &nrChannels, 0); 
+    int width, height, nrComponents;
+    unsigned char *data = stbi_load(filename, &width, &height, &nrComponents, 0); 
+    if (nrComponents == 1)
+        format = GL_RED;
+    else if (nrComponents == 3)
+        format = GL_RGB;
+    else if (nrComponents == 4)
+        format = GL_RGBA;
+
     if (data) {
         // Generate a texture and a mipmap based on the image
-        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, format, GL_UNSIGNED_BYTE, data);
+        glTexImage2D(GL_TEXTURE_2D, 0, format, width, height, 0, format, GL_UNSIGNED_BYTE, data);
         glGenerateMipmap(GL_TEXTURE_2D);
     } else {
         std::cout << "ERROR: Failed to load texture file: " << filename << std::endl;
